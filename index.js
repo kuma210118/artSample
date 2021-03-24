d58cd5282bd0f25e3ff608430efdff9f8aafbb1e
kuma210118 <81290090+kuma210118@users.noreply.github.com>
2021-03-24 20:20:36 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<video autobuffer controls id="mv" width="320"> ▼
+  <source src="CalliWind.mp4">
+  <p>HTML5に対応していません。</p>
+</video>
+
+<script>
+//メディアオブジェクトの取得 ▼
+var media = document.getElementById("mv");
+
+//ループ再生を指定 ▼
+media.loop = true;
+
+</script>
